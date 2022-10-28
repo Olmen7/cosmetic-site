8644c1495b6131f0b16e08d080e9df0fac36e05f
Olmen Vicente <sethvicente1@gmail.com>
2022-10-27 20:27:22 -0700

    fixing contact page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -10,12 +10,18 @@ export const Contact = () => {
         method="POST"
         data-netlify="true"
       >
-        <label className="name">Your Name:</label>
-        <input type="text" name="name" required />
-        <label className="email">Your Email:</label>
-        <input type="email" name="email" required />
+        <label className="name">Name:</label>
+        <input placeholder="name" type="text" name="name" required />
+        <label className="email">Email:</label>
+        <input placeholder="email" type="email" name="email" required />
         <label className="text-area">Message: </label>
-        <textarea cols="39" rows="4" name="message" required></textarea>
+        <textarea
+          placeholder="Type your message here..."
+          cols="39"
+          rows="4"
+          name="message"
+          required
+        ></textarea>
         <div className="button-container">
           <button className="button" type="submit">
             Submit
